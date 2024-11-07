e705f2a37305c4bf54ed2d3eaab0fe69655bc0c6
Kęstutis Žaltauskas <34091448+akaZX@users.noreply.github.com>
2024-11-07 15:26:10 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-[
+--[[
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -992,4 +992,4 @@ require('lazy').setup({
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-
